41051485deb09563b05b9764296a79eade4143ed
ndtao2020 <ndtao2020@gmai.com>
2025-03-06 20:45:15 +0700

    change param type for functions

--- lib/rust.d.ts
+++ lib/rust.d.ts
@@ -2,5 +2,5 @@
 // deno-lint-ignore-file
 // deno-fmt-ignore-file
 
-export function xxh3_64(input: string): bigint;
-export function xxh3_128(input: string): bigint;
+export function xxh3_64(input: Uint8Array): bigint;
+export function xxh3_128(input: Uint8Array): bigint;
--- rs_lib/src/lib.rs
+++ rs_lib/src/lib.rs
@@ -3,11 +3,11 @@ use xxhash_rust::xxh3;
 
 // ================================================= [xxh3] =================================================
 #[wasm_bindgen]
-pub fn xxh3_64(input: &str) -> u64 {
-  xxh3::xxh3_64(input.as_bytes())
+pub fn xxh3_64(input: &[u8]) -> u64 {
+  xxh3::xxh3_64(input)
 }
 
 #[wasm_bindgen]
-pub fn xxh3_128(input: &str) -> u128 {
-  xxh3::xxh3_128(input.as_bytes())
+pub fn xxh3_128(input: &[u8]) -> u128 {
+  xxh3::xxh3_128(input)
 }
